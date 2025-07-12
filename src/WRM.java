655ac9581cb1d3b0eacf6fe99c5f5ba337c3ac85
AHMED ASHFAQ MAHI <24301496@bracu.ac.bd>
2025-07-13 10:20:47 +1400

    Completed Lab Work

--- src/WRM.java
+++ src/WRM.java
@@ -21,29 +21,65 @@ public class WRM {
         tempPatient.next = dh;
 
 
-        }
+
     }
 
     public void servePatient() {
         // To do
+        if (dh.next != dh) {
+            Patient serving = dh.next;
+            System.out.println("Serving paitent " + serving.name);
+            dh.next = serving.next;
+            serving.next.prev = dh;
+        }
+        else {
+            System.out.println("No Patients to serve");
+        }
     }
 
     public void showAllPatient() {
         // To Do
+        Patient pointer = dh.next;
+
+        while (pointer != dh) {
+            System.out.println("Patient ID: " + pointer.id);
+            pointer = pointer.next;
+        }
+
     }
 
     public Boolean canDoctorGoHome() {
         // To Do
-        return null; // Delete this line once you're ready
+        if (dh.next == dh) {
+            return true;
+        }
+        return false; //
     }
 
     public void cancelAll() {
         // To Do
+        dh.next = dh;
+        dh.prev = dh;
     }
 
 
     public void reverseTheLine() {
         // To Do
+        Patient pointer = dh.prev;
+
+        dh.next = pointer;
+
+        while (pointer != dh) {
+            Patient temp = pointer.next;
+            pointer.next = pointer.prev;
+            pointer.prev = temp;
+
+            if (pointer.next == dh) {
+                dh.prev = pointer;
+            }
+            pointer = pointer.next;
+        }
+
     }
 
 }
